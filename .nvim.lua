349de01502052dbd0c43ec33770752a9f3db9707
rootiest <chris@rootiest.com>
2025-05-09 17:00:43 -0400

    chore(yaml): add neovim lua configuration for project
    Add !secret tag to YAML LSP settings in neovim

---
+++ .nvim.lua
@@ -0,0 +1,8 @@
+--          ╭─────────────────────────────────────────────────────────╮
+--          │            Project-specific neovim settings             │
+--          ╰─────────────────────────────────────────────────────────╯
+
+-- YAML LSP Custom Tags
+vim.g.custom_yaml_tags = {
+	"!secret", -- support ESPHome secret tags
+}
